5135223970cb1a3c43b32e65bd0ac425ebe06b26
Mely07 <damely.tineo07@gmail.com>
2020-07-20 22:15:14 -0400

    Done.

--- index.js
+++ index.js
@@ -2,14 +2,33 @@ class Formatter {
   //add static methods here
 
   static capitalize(string) {
-    string.capitalize
+    return string.charAt(0).toUpperCase() + string.slice(1);
   }
 
-  static sanitize() {
-
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '');
   }
 
-  static titleize() {
+  static titleize(sentence) {
+    let splitSent = sentence.split(' ');
+    let lowWords = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    // for (let i = 0; i < splitSent.length; i++) {
+    //   if ((!lowWords.includes(splitSent[i])) || (i == 0)){
+    //     splitSent[i] = (Formatter.capitalize(splitSent[i]))
+    //   }
+    // }    
+    // return splitSent.join(' ');
 
+    let answer = splitSent.map((word, index) => {
+      if (!lowWords.includes(word) || index == 0){
+        return Formatter.capitalize(word);
+      }
+      return word;
+    });
+
+    return answer.join(' ');
   }
+
 }
+
+
