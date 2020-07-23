edeba9e4ab25231ecad4151ebf9649c2e49d16fb
Matthew <mattcorralejo1@gmail.com>
2020-07-23 08:19:08 -0700

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
 class Formatter {
   //add static methods here
+
+ static capitalize(string){
+    let words = string.split(' ')
+    let capitalized = words.map(x => x.charAt(0).toUpperCase() + x.substr(1)).join(' ')
+    return capitalized
+      }
+
+
+
+
+static sanitize(string){
+return string.replace(/[^A-Za-z0-9 '-]/g, '')
+}
+
+static titleize(string){
+ let capitalized = string.charAt(0).toUpperCase() + string.slice(1)
+ let exceptedWords = ["a", "an", "the", "but", "of", "and", "for", "at", "by", "from"]
+ let array = capitalized.split(' ')
+ return array.map(string => {
+   if(exceptedWords.includes(string)){
+   return string}
+   else
+   {return string.charAt(0).toUpperCase() + string.slice(1)
+
+   }
+}).join(' ')
+}
 }
