482adc4520c7e7613948590d5b5a8fd1b803d8d9
asijaw <asija.w@yahoo.com>
2020-07-23 01:05:00 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 class Formatter {
-  //add static methods here
+  static sanitize(name) {
+    return name.replace(/[^A-Za-z0-9- ']+/g, '')
+  }
+
+  static capitalize(name) {
+    return name.charAt(0).toUpperCase() + name.slice(1);
+  }
+
+  static titleize(name) {
+    const dontCap = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let nameArray = name.toLowerCase().split(' ')
+    let capitalized = nameArray.slice(1).map(element => { 
+      return dontCap.includes(element) ? element : this.capitalize(element)})
+    return this.capitalize(nameArray[0]) + ' ' + capitalized.join(' ')
+  }
+
+  
 }
